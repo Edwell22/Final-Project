39d8a6dfb26e077fe29e8b54202725d41e0e2fc4
Edwell22 <113574992+Edwell22@users.noreply.github.com>
2024-04-07 05:18:15 +0200

    Initial commit

---
+++ src/js/product.js
@@ -0,0 +1,10 @@
+import { getParam, showCountItemsInCart} from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
+
+const dataSource = new ProductData('tents');
+
+const productId = getParam('product');
+const product = new ProductDetails(productId, dataSource);
+product.init();
+showCountItemsInCart();
