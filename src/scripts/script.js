d5328ca2523ea448f573cde17b40634f3a4881f8
Edwell22 <edwellkakunguwo358@gmail.com>
2024-04-10 15:46:49 +0200

    new

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,33 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     fetch('recipies.json')
-//         .then(response => response.json())
-//         .then(data => {
-//             const recipesContainer = document.getElementById('recipes');
-//             data.forEach(recipe => {
-//                 const recipeDiv = document.createElement('div');
-//                 recipeDiv.classList.add('recipe');
-
-//                 const nameHeader = document.createElement('h2');
-//                 nameHeader.textContent = recipe.name;
-
-//                 const ingredientsParagraph = document.createElement('p');
-//                 ingredientsParagraph.textContent = 'Ingredients: ' + recipe.ingredients.join(', ');
-
-//                 const instructionsParagraph = document.createElement('p');
-//                 instructionsParagraph.textContent = 'Instructions: ' + recipe.instructions;
-
-//                 recipeDiv.appendChild(nameHeader);
-//                 recipeDiv.appendChild(ingredientsParagraph);
-//                 recipeDiv.appendChild(instructionsParagraph);
-
-//                 recipesContainer.appendChild(recipeDiv);
-//             });
-//         })
-//         .catch(error => console.log('Error fetching recipes:', error));
-// });
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     fetch('recipies.json')
         .then(response => response.json())
